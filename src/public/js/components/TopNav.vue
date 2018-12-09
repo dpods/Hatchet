083c4eca31a45bcb1c6c3a52f53d4138e13fa1fb
David Glassanos <david@naturebox.com>
2018-12-08 16:06:04 -0800

    Removing title from top nav

--- src/public/js/components/TopNav.vue
+++ src/public/js/components/TopNav.vue
@@ -1,20 +1,34 @@
 <template>
     <nav class="navbar navbar-expand fixed-top be-top-header">
         <div class="container-fluid">
-            <div class="be-navbar-header"><a class="navbar-brand" href="index.html"></a>
+            <div class="be-navbar-header">
+                <a class="navbar-brand" href="index.html"></a>
             </div>
             <div class="be-right-navbar">
                 <ul class="nav navbar-nav float-right be-user-nav">
-                    <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-expanded="false"><img src="assets/img/avatar.png" alt="Avatar"><span class="user-name">Túpac Amaru</span></a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <img src="assets/img/avatar.png" alt="Avatar">
+                            <span class="user-name">Túpac Amaru</span>
+                        </a>
                         <div class="dropdown-menu" role="menu">
                             <div class="user-info">
                                 <div class="user-name">Túpac Amaru</div>
                                 <div class="user-position online">Available</div>
-                            </div><a class="dropdown-item" href="pages-profile.html"><span class="icon mdi mdi-face"></span>Account</a><a class="dropdown-item" href="#"><span class="icon mdi mdi-settings"></span>Settings</a><a class="dropdown-item" href="pages-login.html"><span class="icon mdi mdi-power"></span>Logout</a>
+                            </div>
+                            <a class="dropdown-item" href="pages-profile.html">
+                                <span class="icon mdi mdi-face"></span>Account
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                <span class="icon mdi mdi-settings"></span>Settings
+                            </a>
+                            <a class="dropdown-item" href="pages-login.html">
+                                <span class="icon mdi mdi-power"></span>Logout
+                            </a>
                         </div>
                     </li>
                 </ul>
-                <div class="page-title"><span>Dashboard</span></div>
+                <div class="page-title"><span></span></div>
                 <ul class="nav navbar-nav float-right be-icons-nav">
                     <li class="nav-item dropdown"><a class="nav-link be-toggle-right-sidebar" href="#" role="button" aria-expanded="false"><span class="icon mdi mdi-settings"></span></a></li>
                     <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-expanded="false"><span class="icon mdi mdi-notifications"></span><span class="indicator"></span></a>
