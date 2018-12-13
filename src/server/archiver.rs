22fbd0cb0dd28c063c6cf4d28f40362e076bb211
David Glassanos <david@naturebox.com>
2018-12-12 23:31:03 -0800

    archive files by the hour

--- src/server/archiver.rs
+++ src/server/archiver.rs
@@ -1,68 +1,82 @@
-use std::fs::File;
 use std::fs::OpenOptions;
 use std::io;
 use std::io::prelude::*;
 use std::sync::{Arc, Mutex};
 use chrono::prelude::*;
-use std::fs;
-use std::path::Path;
+use std::fs::{self, DirBuilder};
+
+const FILENAME: &str = "archive.log";
 
 pub struct Archiver {
-    filepath: String,
-    filename: String,
-    file: File,
+    archive_dir: String,
+    utc: DateTime<Utc>,
 }
 
 impl Archiver {
-    pub fn new(path: &String) -> Result<Arc<Mutex<Archiver>>, io::Error> {
-        // First, create archive directory if it does not exist
-        fs::create_dir_all(path.clone())?;
-
-        let filepath = path.clone();
-        let filename = Archiver::current_filename();
-        let file = Archiver::open_file(&filepath, &filename);
+    pub fn new(archive_dir: &String) -> Result<Arc<Mutex<Archiver>>, io::Error> {
+        let archive_dir = archive_dir.clone();
 
         let archiver = Archiver {
-            filepath: filepath,
-            filename: filename,
-            file: file
+            archive_dir: archive_dir,
+            utc: Utc::now(),
         };
 
         Ok(Arc::new(Mutex::new(archiver)))
     }
 
-    pub fn open_file(filepath: &String, filename: &String) -> File {
-        // The open file for archiving and return an arc mutex to it since multiple threads
-        // will be writing to this file
-        match OpenOptions::new()
-            .create(true)
-            .write(true)
-            .append(true)
-            .open(format!("{}/{}", filepath, filename)) {
-                Err(e) => panic!("Couldn't open file for archiving: {}", e),
-                Ok(x) => x,
-            }
+    fn current_year(&self) -> i32 {
+        self.utc.year()
     }
 
-    pub fn current_filename() -> String {
-        let dt = Local::now();
-        format!("{}{}{}.log", dt.year(), dt.month(), dt.day())
+    fn current_month(&self) -> u32 {
+        self.utc.month()
+    }
+
+    fn current_day(&self) -> u32{
+        self.utc.day()
+    }
+
+    fn current_hour(&self) -> u32 {
+        self.utc.hour()
+    }
+
+    fn current_directory(&self) -> String {
+        format!(
+            "{}/{}/{}/{}/{}",
+            self.archive_dir,
+            self.current_year(),
+            self.current_month(),
+            self.current_day(),
+            self.current_hour(),
+        )
+    }
+
+    fn current_filepath(&self) -> String {
+        format!(
+            "{}/{}",
+            self.current_directory(),
+            FILENAME
+        )
     }
 
     pub fn archive(&mut self, line: &str) {
-        let current_filename = Archiver::current_filename();
-        let file_exists = Path::new(&self.get_fullpath()).exists();
+        println!("{}", self.current_directory());
 
-        if !file_exists || current_filename != self.filename {
-            self.file = Archiver::open_file(&self.filepath, &current_filename);
-        }
+        let path = self.current_directory();
 
-        if let Err(e) = writeln!(self.file, "{}", line) {
+        DirBuilder::new()
+            .recursive(true)
+            .create(&path).unwrap();
+
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(self.current_filepath())
+            .expect("Couldn't open file for archiving");
+
+        if let Err(e) = writeln!(file, "{}", line) {
             eprintln!("Couldn't write to file: {}", e);
         }
-    }
-
-    fn get_fullpath(&self) -> String {
-        format!("{}/{}", self.filepath, self.filename)
     }
 }
